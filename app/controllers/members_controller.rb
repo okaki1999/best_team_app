11c76df9043127044a2434169623b3d6257cfb59
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-02-03 13:07:55 +0900

    結果表示機能実装

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -15,14 +15,56 @@ class MembersController < ApplicationController
             redirect_to action: :new
         end
     end
+# members_controller.rb (もしくは該当のコントローラー)
+
+def update
+    @member = Member.find(params[:id])
   
-    def destroy
-        @member = Member.find(params[:id])
-        @member.destroy
-        redirect_to root_path, notice: 'Member was successfully deleted.'
+    if @member.update(member_params)
+      flash[:notice] = 'メンバー情報を更新しました。'
+      
+      # プルダウンで選択された結果を元に、レーティングの更新を行う
+      update_rating_based_on_result(params[:member][:result])
+  
+      redirect_to @member
+    else
+      flash[:alert] = '更新に失敗しました。'
+      render :edit
     end
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to root_path, notice: 'Member was successfully deleted.'
+    end
+
   
-    private
+  private
+  
+  def member_params
+    params.require(:member).permit(:name, :rating)
+  end
+  
+  def update_rating_based_on_result(result)
+    winner, loser = result.split('、')
+    winner_member = Member.find_by(name: winner)
+    loser_member = Member.find_by(name: loser)
+  
+    winner_rate = winner_member.rating
+    loser_rate = loser_member.rating
+  
+    # calculatePointメソッドはレーティングの差から新しいポイントを計算するものと仮定
+    new_rating_for_winner = calculatePoint(winner_rate, loser_rate)
+    new_rating_for_loser = calculatePoint(loser_rate, winner_rate)
+  
+    # データベースの更新
+    winner_member.update(rating: new_rating_for_winner)
+    loser_member.update(rating: new_rating_for_loser)
+  end
+  
+  
+
   
     def member_params
       params.require(:member).permit(:name, :rating)
