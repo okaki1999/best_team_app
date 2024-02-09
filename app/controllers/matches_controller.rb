a9a3de599b82ab8c55522119b554a7e4caa2a2fb
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-02-10 01:08:58 +0900

    各ページざっくりとのレイアウト

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -16,38 +16,35 @@ class MatchesController < ApplicationController
         @match = Match.new
     end
 
-def create
-  @match = Match.new(match_params)
-  p match_params.inspect
-
-  @members = Member.all
-  @member_ids = @members.select(&:already_participation?).map(&:id)
-
-  # トランザクションを開始
-  ActiveRecord::Base.transaction do
-    # まずは試合を保存
-    if @match.save
-      # 試合が保存されたら、各メンバーを試合に参加させる
-      @member_ids.each do |member_id|
-        @enrollment = Enrollment.new(member_id: member_id, match_id: @match.id)
-        @enrollment.save!
-      end
-      # リダイレクト時に試合のIDが必要なので保存後にIDを取得
-      match_id = @match.id
-
-      # トランザクションの中でリダイレクトするのは適切でないため、トランザクションの外側に移動
-    else
-      # 試合の保存に失敗した場合の処理
-      # エラーメッセージを表示したり、必要に応じて処理を行う
-      render :new
-      return
+    def create
+        @match = Match.new(match_params)
+        p match_params.inspect
+      
+        @members = Member.all
+        @member_ids = @members.select(&:already_participation?).map(&:id)
+      
+        # トランザクションを開始
+        ActiveRecord::Base.transaction do
+            # まずは試合を保存
+            if @match.save
+                # 試合が保存されたら、各メンバーを試合に参加させる
+                @member_ids.each do |member|
+                    @enrollment = Enrollment.new(member_id: member, match_id: @match.id)
+                    @enrollment.save!
+                end
+                # リダイレクト時に試合のIDが必要なので保存後にIDを取得
+                match_id = @match.id
+            else
+                # 試合の保存に失敗した場合の処理
+                # エラーメッセージを表示したり、必要に応じて処理を行う
+                redirect_to action: :new
+            return
+            end
+      
+            # リダイレクト
+            redirect_to match_path(match_id), notice: '新しい試合が作成されました。'
+        end
     end
-  end
-
-  # リダイレクト
-  redirect_to match_path(match_id), notice: '新しい試合が作成されました。'
-end
-
 
     def clamp(point, min, max)
         if point < min
@@ -80,7 +77,7 @@ end
             Member.where(id: [member_ids.first, member_ids.second]).update_all("rating = rating + #{rating_change}")
             Member.where(id: [member_ids.fourth, member_ids.third]).update_all("rating = rating - #{rating_change}")
         end
-        redirect_to root_path
+        
     end
   
 
