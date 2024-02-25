db0173d215bfbb74deedddca52275f5ba8526738
okaki1999 <140598093+okaki1999@users.noreply.github.com>
2024-02-25 15:36:34 +0900

    バスケ得点率順に変更

--- app/models/bsk_member.rb
+++ app/models/bsk_member.rb
@@ -3,6 +3,12 @@ class BskMember < ApplicationRecord
     has_many :bsk_enrollments, dependent: :destroy
     belongs_to :bsk_match, optional: true
     attribute :total_match, :integer, default: 0
+    
+    def point_per_game
+      return 0 if total_match == 0  # ゼロ除算を防ぐために、total_match が 0 の場合は 0 を返す
+  
+      scoring_rate.to_f / total_match
+    end
   
     def bsk_already_participation?
       bsk_participations.exists?(bsk_member_id: id)
